42d6233682d2189973cdab8801f2b6c5b7e3da9f
Akios <akiosdev6@gmail.com>
2022-02-25 23:50:54 -0300

    Adding main-feed component
    Added sass & react-icons

---
+++ src/components/feedItem.tsx
@@ -0,0 +1,49 @@
+import { FiExternalLink } from "react-icons/fi";
+
+export interface feedItemType {
+    title: string
+    prevPrice: string
+    goToUrl: string
+    expirationDate: string
+    billStatus: string
+}
+
+interface proptype {
+    itemProps: feedItemType
+}
+
+
+function FeedItem({ itemProps }: proptype) {
+    const itemClass = 'feed-item '+ itemProps.billStatus
+
+    return (
+        <div className={itemClass}>
+            <div className="item-header">
+                <div className="title-section">
+                    <h1 className="feed-item-title">{itemProps.title}</h1>
+                    <FiExternalLink />
+                </div>
+                <div className="expiration-section">
+                    <p className="expiration-label">Due</p>
+                    <p className="expiration">{itemProps.expirationDate}</p>
+                </div>
+            </div>
+            <div className="item-body">
+                <div className="current-price-section">
+                    <p className="current-price-label">current price:</p>
+                    <input className="current-price-input" type="text" name="current-price" id="current-price" />
+                </div>
+                <div className="previous-price-section">
+                    <p className="previous-price-label">Previous Price:</p>
+                    <p className="previous-price">{itemProps.prevPrice}</p>
+                </div>
+            </div>
+            <div className="item-lower">
+
+                <button className="pay-bill">PAY BILL</button>
+            </div>
+        </div>
+    );
+}
+
+export default FeedItem;
