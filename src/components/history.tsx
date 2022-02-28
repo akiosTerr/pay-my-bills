858d45c9b12d90f1903a2f8b2990b887184fb520
Akios <akiosdev6@gmail.com>
2022-02-28 14:49:27 -0300

    History component temporary style

--- src/components/history.tsx
+++ src/components/history.tsx
@@ -1,6 +1,6 @@
 import HistoryItem, { HistoryItemType } from "./historyItem";
 import '../style/history.scss'
-import Select from 'react-select'
+// import Select from 'react-select'
 
 
 function History() {
@@ -16,12 +16,12 @@ function History() {
             date: '15/02/2022'
         }
     ]
-    const monthOptions = ['July', 'August'];
+    // const monthOptions = ['July', 'August'];
 
     return ( 
         <div className="history">
             <h1 className="history-title">
-                Bill History
+                Payment History
             </h1>
             {/* <Select options={monthOptions} /> */}
             <div className="history-array">
--- src/components/historyItem.tsx
+++ src/components/historyItem.tsx
@@ -13,8 +13,18 @@ function HistoryItem({historyItemProp}:PropType) {
     return ( 
         <div className="history-item">
             <h2 className="title">{historyItemProp.title}</h2>
-            <p className="date">{historyItemProp.date}</p>
-            <p className="value">{historyItemProp.value}</p>
+            <div className="value-section">
+                <h2 className="value">{historyItemProp.value}</h2>
+            </div>
+            <div className="paid-section">
+                <p className="paid-label">Paid in:</p>
+                <p className="date">{historyItemProp.date}</p>
+            </div>
+            <div className="exp-section">
+                <p className="exp-label">Expired in:</p>
+                <p className="date">{historyItemProp.date}</p>
+            </div>
+            
         </div>
      );
 }
