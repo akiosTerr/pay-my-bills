2ed52efaf4e7b265bf5d4b1cc9244c41e4a99f9e
akiosTerr <stereoblindstudio@gmail.com>
2024-02-13 20:05:34 -0300

    replace BrowserRouter with HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import { SetAuthCtx, SetProfileNameCtx } from "Contexts";
 import { useAuth } from "hooks/auth_hook";
 import { useState } from "react";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Route, Routes, HashRouter as Router } from "react-router-dom";
 import NavBar from "./components/navBar";
 import AddItemForm from "./pages/addItemForm";
 import ChartPage from "./pages/chartPage";
 import EditItemForm from "./pages/editItemForm";
 import Mainfeed from "./pages/mainfeed";
 import "./style/base.scss";
-import LoginForm from "pages/loginForm";
 
 function App() {
   const [profileName, setProfileName] = useState<string>("");
@@ -22,7 +21,6 @@ function App() {
             <NavBar></NavBar>
             <Routes>
               <Route path="/" element={<Mainfeed />} />
-              <Route path="/login" element={<LoginForm login={login} />} />
               <Route path="/add" element={<AddItemForm />} />
               <Route path="/edit/:id" element={<EditItemForm />} />
               <Route path="/chart" element={<ChartPage />} />
